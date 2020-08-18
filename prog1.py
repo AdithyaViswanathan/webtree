dd3708e019b7bfb9c8d4a5c5135d2f585cc40b47
Adithya Viswanathan <adith_viswa@ymail.com>
2020-08-18 19:04:02 +0530

    Fixed Label Issue

--- prog1.py
+++ prog1.py
@@ -1,61 +1,154 @@
 # https://plotly.com/python/reference/#scatter
 # https://networkx.github.io/documentation/stable/tutorial.html
 
-
 import networkx as nx
 import matplotlib.pyplot as plt
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # To get everything
 import chart_studio.plotly as py
 from plotly.graph_objs import *
 
-output = "<html><head></head><body><div></div><p></p></body></html>"
+output = "<html><head></head><body><div></div><p></p></body></html>" #Output to be processed
 soup = BeautifulSoup(output, 'lxml')
-result = soup.findAll("html")
+result = soup.findAll("html") #The result will point to the top node <html>
+"""
+The findAll method traverses the tree, starting at the given point, and 
+finds all the Tag and NavigableString objects that match the criteria you give. 
+
+"""
 
 
+G = nx.DiGraph() #Empty Graph with no nodes and no edges.
 
-G = nx.DiGraph()
-
-G.add_node(result[0].name)
-parent = result[0]
-parents = [parent]
-labels=[parent.name]
+G.add_node(result[0].name) 	# result[0].name --> html
+parent = result[0] 			#<html><head></head><body><div></div><p></p></body></html>
+parents = [parent] 			#[<html><head></head><body><div></div><p></p></body></html>]
+labels=[parent.name] 		#['html']
 edges = []
 for parent in parents:
+	"""This loop will have 
+	<tags> [Contents inside ... incl. child 
+	<childtag></childtag>] </tags>... Then goes on to explore <childtags>
+	because of Line#53
+	"""
 	for child in parent.contents:
+		"""
+		Four iterations of the loop with print(child)
+		<head></head>
+		<body><div></div><p></p></body>
+		<div></div>
+		<p></p>
+		"""
 		G.add_node(child.name)
 		G.add_edge(parent.name,child.name)
 		x = (parent.name,child.name)
+		"""
+		Contents of X 4 Iterations
+		('html', 'head')
+		('html', 'body')
+		('body', 'div')
+		('body', 'p')
+		"""
 		labels.append(child.name)
 		edges.append(x)
 		parents.append(child)
 
+"""
+print(parents)
+
+[<html><head></head><body><div></div><p></p></body></html>, 
+<head></head>, <body><div></div><p></p></body>, <div></div>, <p></p>]
+
+print(edges)
+
+[('html', 'head'), ('html', 'body'), ('body', 'div'), ('body', 'p')]
+-
+"""
+
 pos = nx.spiral_layout(G)
+
+
+"""
+spiral_layout(G, scale=1, center=None, dim=2, resolution=0.35, equidistant=False)
+
+G (NetworkX graph or list of nodes) – A position will be assigned to every node in G.
+scale (number (default: 1)) 		– Scale factor for positions.
+center (array-like or None) 		– Coordinate pair around which to center the layout.
+dim (int) 							– Dimension of layout, currently only dim=2 is supported. Other dimension values result in a ValueError.
+resolution (float) 					– The compactness of the spiral layout returned. Lower values result in more compressed spiral layouts.
+equidistant (bool) 					– If True, nodes will be plotted equidistant from each other.
+
+"""
+
+
 # nx.draw(G,pos,with_labels=True, font_weight='bold')
 # print(parents)
 # plt.show()
+
 g=nx.Graph()
 g.add_nodes_from(parents)
-g.add_edges_from(edges)# E is the list of edges
+g.add_edges_from(edges) # E is the list of edges
 
 pos=nx.fruchterman_reingold_layout(g)
 
+"""
+Example print(pos) #They vary each time
 
-N = len(parents)
+{
+ <html><head></head><body><div></div><p></p></body></html>: array([-0.13771074,  0.92185995]),
+ <head></head>: array([ 0.93064586, -0.40409817]),
+ <body><div></div><p></p></body>: array([0.48708524, 0.72255242]), 
+ <div></div>: array([-0.71727669,  0.71954607]), 
+ <p></p>: array([-1.        ,  0.09307196]), 
+ 'html': array([-0.01867046, -0.41227352]), 
+ 'head': array([-0.14941259, -0.46630504]), 
+ 'body': array([ 0.1421822 , -0.38726024]), 
+ 'div': array([ 0.23439108, -0.283171  ]), 
+ 'p': array([ 0.22876609, -0.50392243])
+ }
 
-Xv=[pos[k][0] for k in pos.keys()]
-Yv=[pos[k][1] for k in pos.keys()]
+"""
+
+"""
+# This part of code eliminates the extra nodes that are present in the graph
+# I didnt know if you want those extra nodes or not..
+
+N = len(parents) # ?
+counter = 0
+Xv = []
+Yv = []
+for k in pos.keys():
+	if(counter>=N):
+		Xv.append(pos[k][0])
+		Yv.append(pos[k][1])
+	counter+=1
+"""
+
+
+"""
+['html', 'head', 'body', 'div', 'p'] This was the initial label
+"""
+Xv=[pos[k][0],for k in pos.keys()] # X-coords from the above pos
+Yv=[pos[k][1],for k in pos.keys()] # Y-coords from the above pos
+labels = labels + labels 
+
+"""
+After I did this ... the labels turned out to be
+['html', 'head', 'body', 'div', 'p', 'html', 'head', 'body', 'div', 'p'] 
+So now everything is getting marked ..
+We have to either take the extra tags from pos out or do this 
+"""
+
 Xed=[]
 Yed=[]
 for edge in edges:
-    Xed+=[pos[edge[0]][0],pos[edge[1]][0], None]
-    Yed+=[pos[edge[0]][1],pos[edge[1]][1], None]
+	Xed+=[pos[edge[0]][0],pos[edge[1]][0],None]
+	Yed+=[pos[edge[0]][1],pos[edge[1]][1],None]
 
 trace3=Scatter(x=Xed,
                y=Yed,
                mode='lines',
                line=dict(color='rgb(210,210,210)', width=1),
-               hoverinfo='none'
+               hoverinfo='text'
                )
 trace4=Scatter(x=Xv,
                y=Yv,
@@ -117,5 +210,4 @@ layout=Layout(title= "Just Checking",
 
 data1=[trace3, trace4]
 fig1=Figure(data=data1, layout=layout)
-# fig1['layout']['annotations'][0]['text']=annot
 fig1.write_html('first_figure.html', auto_open=True)
